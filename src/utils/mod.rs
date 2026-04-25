009fa6d9ed0968ca02c8d6f0727860617d954d7c
Keeze392 <94383342+Keeze392@users.noreply.github.com>
2026-04-25 14:08:33 +0200

    change some mod.rs
    add target_os for audio

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,8 +3,14 @@ pub mod ui_settings;
 
 pub mod stt;
 
-pub mod audio_cpal;
-pub mod audio;
+#[cfg(target_os = "linux")]
+pub mod audio_linux;
+
+#[cfg(target_os = "windows")]
+pub mod audio_windows;
+
+#[cfg(target_os = "macos")]
+pub mod audio_macos;
 
 #[cfg(feature = "osc")]
 pub mod osc;
