8590abac6d32e9d2415d95a96629ae9a02879676
Keeze392 <Keeze392@noreply.codeberg.org>
2026-07-13 20:22:43 +0200

    refacoring
    from old messy to clean impl and no longer require separate Thread.

--- src/utils/osc.rs
+++ src/utils/osc.rs
@@ -1,43 +1,133 @@
 use rosc::{OscMessage, OscPacket, OscType, encoder};
-use std::{net::UdpSocket, sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}, mpsc}, thread::sleep, time::Duration};
+use std::{net::UdpSocket, sync::{Arc, Mutex}};
 
-pub fn osc_sender_string(
-    text: mpsc::Receiver<String>,
-    is_should_close: Arc<AtomicBool>,
-    osc_output_path: Arc<Mutex<String>>,
-    osc_output_port: Arc<Mutex<String>>,
-    ) {
+pub struct OSCSender {
+    osc_output_path: String,
+    osc_output_port: String,
 
-    // init
-    let target_addr: String = String::from("127.0.0.1:");
+    socket: UdpSocket,
+}
 
-    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
-    sleep(Duration::from_millis(1000));
+impl OSCSender {
+    pub fn new(arc_output_path: &Arc<Mutex<String>>, arc_output_port: &Arc<Mutex<String>>) -> Self {
+        let output_path = arc_output_path.lock().unwrap().clone();
+        let output_port = arc_output_port.lock().unwrap().clone();
 
-    while !is_should_close.load(Ordering::Relaxed) {
-        let port = osc_output_port.lock().unwrap().clone();
-        let target_addr_port = format!("{}{}", target_addr, port);
+        Self {
+            osc_output_path: output_path,
+            osc_output_port: output_port,
 
-        let addr_path_sender = osc_output_path.lock().unwrap().clone();
-    
-        let output_text: String = match text.recv() {
-            Ok(s) => s,
-            Err(_) => break,
-        };
+            socket: UdpSocket::bind("127.0.0.1:0").expect("OSC Err -- socket bind local ip failed"),
+        }
+    }
 
+    pub fn set_path(&mut self, arc_output_path: &Arc<Mutex<String>>) {
+        self.osc_output_path = arc_output_path.lock().unwrap().clone();
+    }
+
+    pub fn set_port(&mut self, arc_output_port: &Arc<Mutex<String>>) {
+        self.osc_output_port = arc_output_port.lock().unwrap().clone();
+    }
+
+    // for sending with path and port to local ip only
+    pub fn send(&self, text: String) {
         let msg = OscMessage {
-            addr: addr_path_sender.clone(),
-            args: vec![OscType::String(output_text.clone())],
+            addr: self.osc_output_path.clone(),
+            args: vec![OscType::String(text)]
         };
 
         let packet = OscPacket::Message(msg);
+
         let buf = match encoder::encode(&packet) {
-            Ok(b) => b,
-            Err(e) => { println!("Error -- make packet failed: {e}"); return; }
+            Ok(val) => val,
+            Err(e) => { eprintln!("OSC Err -- encoding a packet failed: {e}"); return; }
         };
 
-        if !port.is_empty() {
-            socket.send_to(&buf, target_addr_port).unwrap();
+        if !self.osc_output_port.is_empty() {
+            let full_addr_target = format!("127.0.0.1:{}", self.osc_output_port);
+
+            match self.socket.send_to(&buf, full_addr_target) {
+                Ok(_) => (),
+                Err(e) => { eprintln!("OSC Err -- Sending failed: {e}"); return; }
+            }
         }
+    }
+
+    // plan to add for vrc in future
+    pub fn send_to_vrc(&self) {
+        unimplemented!()
+    }
+}
+
+impl Default for OSCSender {
+    fn default() -> Self {
+        Self {
+            osc_output_path: String::new(),
+            osc_output_port: String::new(),
+
+            socket: UdpSocket::bind("127.0.0.1:0").expect("OSC Err -- socket bind local ip failed"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn set_path_test() {
+        let mut osc_struct_test = OSCSender::new(
+            &Arc::new(Mutex::new("".into())),
+            &Arc::new(Mutex::new("".into()))
+        );
+
+        assert_eq!(String::from(""), osc_struct_test.osc_output_path);
+
+        osc_struct_test.set_path(&Arc::new(Mutex::new("/blah_blah".into())));
+        
+        assert_eq!(String::from("/blah_blah"), osc_struct_test.osc_output_path);
+    }
+
+    #[test]
+    fn set_port_test() {
+        let mut osc_struct_test = OSCSender::new(
+            &Arc::new(Mutex::new("".into())),
+            &Arc::new(Mutex::new("".into()))
+        );
+
+        assert_eq!(String::from(""), osc_struct_test.osc_output_port);
+
+        osc_struct_test.set_port(&Arc::new(Mutex::new("9009".into())));
+        
+        assert_eq!(String::from("9009"), osc_struct_test.osc_output_port);
+    }
+
+    // i don't know if this is good idea or design
+    // just checking is ipv4 if true meaning it's available i guess?
+    #[test]
+    fn socket_test() {
+        let osc_struct_test = OSCSender::new(
+            &Arc::new(Mutex::new("".into())),
+            &Arc::new(Mutex::new("".into()))
+        );
+
+        assert!(osc_struct_test.socket.local_addr().unwrap().is_ipv4());
+    }
+
+    #[test]
+    #[ignore]
+    fn send_test() {
+        let osc_struct_test = OSCSender::new(
+            &Arc::new(Mutex::new("/say_hi".into())),
+            &Arc::new(Mutex::new("9005".into()))
+        );
+
+        osc_struct_test.send("test test".into());
+    }
+
+    #[test]
+    #[ignore]
+    fn send_to_vrc_test() {
+
     }
 }
