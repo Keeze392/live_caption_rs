b2e03e18df5575caa7ff7f348b5a51b89e793711
Keeze392 <Keeze392@noreply.codeberg.org>
2026-07-13 20:30:59 +0200

    refactoring main
    removing mass variables and move inside function instead.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ mod utils;
 
 use eframe::egui;
 
-#[cfg(feature = "osc")]
 use crate::utils::osc;
-
 use crate::utils::ui;
 use crate::utils::stt;
 
 #[cfg(target_os = "linux")]
 use crate::utils::audio_linux::{audio_worker, get_devices_array};
 
+// not planned to, so idk when for windows and macos.
 #[cfg(target_os = "windows")]
 use crate::utils::audio_windows::audio_worker;
 
 #[cfg(target_os = "macos")]
 use crate::utils::audio_macos::audio_worker;
+
+use crate::osc::OSCSender;
+use crate::utils::ui_settings::LiveCaptionSettingsRs;
 
 use std::thread;
 use std::sync::{mpsc, Arc, Mutex, atomic::AtomicBool};
@@ -23,70 +25,53 @@ use std::sync::{mpsc, Arc, Mutex, atomic::AtomicBool};
 fn main() {
     env_logger::init();
 
-    // vector mpsc channel
+    // vector mpsc channel from audio to Whisper
     let (tx, rx) = mpsc::sync_channel::<Vec<f32>>(16);
 
-    #[cfg(feature = "osc")]
-    let (output_text_tx, output_text_rx) = mpsc::sync_channel::<String>(8);
+    // bool, if UI is closed, send flag to all thread to stop from loop
+    let is_ui_closed = Arc::new(AtomicBool::new(false));
 
-    // Arc for safety shared data between Threads
-    // bool
-    let is_ui_closed_t1 = Arc::new(AtomicBool::new(false));
-    let is_ui_closed_t2 = Arc::clone(&is_ui_closed_t1);
-    let is_ui_closed_t3 = Arc::clone(&is_ui_closed_t1);
-    #[cfg(feature = "osc")]
-    let is_ui_closed_t4 = Arc::clone(&is_ui_closed_t1);
+    // String from whisper to UI label
+    let text_shared = Arc::new(Mutex::new(String::new()));
+    let text_shared_history = Arc::new(Mutex::new(String::new()));
 
-    // String
-    let text_shared_t2 = Arc::new(Mutex::new(String::new()));
-    let text_shared_t3 = Arc::clone(&text_shared_t2);
+    // select model for whipser
+    let select_model = Arc::new(Mutex::new(None));
 
-    let text_shared_history_t2 = Arc::new(Mutex::new(String::new()));
-    let text_shared_history_t3 = Arc::clone(&text_shared_history_t2);
-
-    // select model
-    let select_model_t2 = Arc::new(Mutex::new(None));
-    let select_model_t3 = Arc::clone(&select_model_t2);
-
-    // Transparent
-    let transparent_value_t3 = Arc::new(Mutex::new(1.0));
+    // Transparent for UI background
+    let transparent_value = Arc::new(Mutex::new(1.0));
 
     // audio devices
-    let devices_t3 = Arc::new(Mutex::new(Vec::new()));
+    let devices = Arc::new(Mutex::new(Vec::new()));
 
-    get_devices_array(Arc::clone(&devices_t3));
-
-    let device_selected_t1 = Arc::new(Mutex::new(Option::<String>::None));
-    let device_selected_t3 = Arc::clone(&device_selected_t1);
+    // fill the devices list to choose a device
+    get_devices_array(Arc::clone(&devices));
+    let device_selected = Arc::new(Mutex::new(Option::<String>::None));
 
     // a bool for restart audio to change select device
-    let should_restart_audio_t3 = Arc::new(AtomicBool::new(false));
+    // it will restart if user choose different a device to listen
+    let should_restart_audio = Arc::new(AtomicBool::new(false));
 
     // a bool for audio tell main thread that audio thread has exited
     // then main thread can spawn new thread in order to avoid race condition
-    let thread_exited_ready_t3 = Arc::new(AtomicBool::new(false));
+    let thread_exited_ready = Arc::new(AtomicBool::new(false));
 
     // String osc
-    #[cfg(feature = "osc")]
-    let osc_output_path_t3 = Arc::new(Mutex::new(String::new()));
-    #[cfg(feature = "osc")]
-    let osc_output_path_t4 = Arc::clone(&osc_output_path_t3);
+    let osc_output_path = Arc::new(Mutex::new(String::new()));
+    let osc_output_port = Arc::new(Mutex::new(String::new()));
 
-    #[cfg(feature = "osc")]
-    let osc_output_port_t3 = Arc::new(Mutex::new(String::new()));
-    #[cfg(feature = "osc")]
-    let osc_output_port_t4 = Arc::clone(&osc_output_port_t3);
+    // stt - Whisper
+    let stt_text_shared = Arc::clone(&text_shared);
+    let stt_text_shared_history = Arc::clone(&text_shared_history);
+    let stt_is_ui_closed = Arc::clone(&is_ui_closed);
+    let stt_select_model = Arc::clone(&select_model);
 
-    // add task for speech to text
-    // t2
     let stt_thread = thread::spawn(move || stt::worker(
             rx,
-            text_shared_t2,
-            text_shared_history_t2,
-            is_ui_closed_t2,
-            select_model_t2,
-            #[cfg(feature = "osc")]
-            output_text_tx,
+            stt_text_shared,
+            stt_text_shared_history,
+            stt_is_ui_closed,
+            stt_select_model,
     ));
 
     let native_options = eframe::NativeOptions {
@@ -100,16 +85,22 @@ fn main() {
         ..Default::default()
     };
 
-    // t4
-    #[cfg(feature = "osc")]
-    let osc_thread = thread::spawn(move || osc::osc_sender_string(
-            output_text_rx,
-            is_ui_closed_t4,
-            osc_output_path_t4,
-            osc_output_port_t4
-    ));
+    let live_caption_settings = LiveCaptionSettingsRs::new(
+        Arc::clone(&select_model),
+        transparent_value,
+        Arc::clone(&osc_output_path),
+        Arc::clone(&osc_output_port),
+        devices,
+        device_selected,
+        should_restart_audio,
+        thread_exited_ready
+    );
 
-    // t3
+    let osc_sender = OSCSender::new(
+        &osc_output_path,
+        &osc_output_port
+    );
+
     match eframe::run_native(
         "Live Caption",
         native_options, 
@@ -118,21 +109,13 @@ fn main() {
                 Box::new({
                     egui_extras::install_image_loaders(&cc.egui_ctx);
                     ui::LiveCaptionRs::new(
-                        cc,
-                        text_shared_t3,
-                        text_shared_history_t3,
-                        #[cfg(feature = "osc")]
-                        osc_output_path_t3,
-                        #[cfg(feature = "osc")]
-                        osc_output_port_t3,
-                        select_model_t3,
-                        transparent_value_t3,
-                        is_ui_closed_t3,
-                        devices_t3,
-                        device_selected_t3,
-                        should_restart_audio_t3,
-                        tx,
-                        thread_exited_ready_t3,
+                        cc, 
+                        text_shared,
+                        text_shared_history,
+                        is_ui_closed,
+                        tx, 
+                        live_caption_settings,
+                        osc_sender
                     )
                 })
             )
@@ -145,11 +128,5 @@ fn main() {
     match stt_thread.join() {
         Ok(()) => (),
         Err(e) => panic!("STT Thread error: {e:?}"),
-    };
-
-    #[cfg(feature = "osc")]
-    match osc_thread.join() {
-        Ok(()) => (),
-        Err(e) => panic!("OSC Thread error: {e:?}"),
     };
 }
