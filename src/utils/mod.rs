86270dfe410aae7b6ec1fc71689ed0ecfef5810d
Keeze392 <94383342+Keeze392@users.noreply.github.com>
2026-08-16 17:12:45 +0200

    Update mod.rs
    removed Macos and Windows as no planned to add support. (forgot! whoops.)

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,11 +4,4 @@ pub mod ui_settings;
 
 pub mod stt;
 
-#[cfg(target_os = "linux")]
 pub mod audio_linux;
-
-#[cfg(target_os = "windows")]
-pub mod audio_windows;
-
-#[cfg(target_os = "macos")]
-pub mod audio_macos;
